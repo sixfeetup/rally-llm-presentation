f952128e2c2be589754502407f813b55f0a315b2
Glenn Franxman <gfranxman@gmail.com>
2023-08-18 16:30:05 -0400

    The following updates have been made to the `as_code_more_control.py` file:
    1. Module `asyncio` has been imported.
    2. Converted the main script into an asynchronous function named `loop()`.
    3. Configuration for logging has been added to show logs if the script is run as the main program.
    4. The function `get_local_credentials()` is now part of the async function `loop()`.
    5. Included optional usage of the Chroma database for persisting the data and reusing the model for repeated queries. This option can be toggled using the `PERSIST` variable.
    6. Handling for model queries has been improved. If passed as an argument while running the script, the first system argument will be used as the model query. If no argument is passed, the user will be asked to input a prompt during runtime.
    7. Instead of using only the `data.txt` file, the data loader now traverses the entire data directory recursively, identifying all `.txt` files as data sources.
    8. Source documents for the retriever are now returned along with the query results.
    9. Debugging information for retrieval has been added, which prints found documents in the console.
    10. Added the usage of `amax_marginal_relevance_search` function for retrieving the documents.
    11. Improved the retrieval of documents. Now it uses `run_manager` from the `callback_manager` to retrieve the documents instead of directly using the retriever.
    12. Now, it is also compatible with asyncio by using `asyncio.run()` to start the main event loop. Similarly, the `loop()` function is also asynchronous now.

--- as_code_more_control.py
+++ as_code_more_control.py
@@ -1,4 +1,5 @@
 #! env python
+import asyncio
 import json
 import os
 import subprocess
@@ -16,45 +17,81 @@ from langchain.vectorstores import Chroma
 
 
 def get_local_credentials():
-  keypath = subprocess.check_output(".venv/bin/llm keys path".split(), text=True).strip()
-  print(keypath)
-  with open(keypath) as f:
-    return json.load(f)["openai"]
+    keypath = subprocess.check_output(
+        ".venv/bin/llm keys path".split(), text=True
+    ).strip()
+    print(keypath)
+    with open(keypath) as f:
+        return json.load(f)["openai"]
 
-os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", get_local_credentials())
 
-# Enable to save to disk & reuse the model (for repeated queries on the same data)
-PERSIST = False
+async def loop(k=3):
+    os.environ["OPENAI_API_KEY"] = os.environ.get(
+        "OPENAI_API_KEY", get_local_credentials()
+    )
 
-query = None
-if len(sys.argv) > 1:
-  query = sys.argv[1]
+    # Enable to save to disk & reuse the model (for repeated queries on the same data)
+    # PERSIST = True
+    PERSIST = False
 
-if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+    query = None
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
 
-chain = ConversationalRetrievalChain.from_llm(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
+    # loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+    loader = DirectoryLoader("data/", recursive=True, show_progress=True, glob="*.txt")
 
-chat_history = []
-while True:
-  if not query:
-    query = input("Prompt: ")
-  if query in ['quit', 'q', 'exit']:
-    sys.exit()
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
 
-  chat_history.append((query, result['answer']))
-  query = None
+        vectorstore = Chroma(
+            persist_directory="persist", embedding_function=OpenAIEmbeddings()
+        )
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore).from_loaders([loader])
+    else:
+        index = VectorstoreIndexCreator().from_loaders([loader])
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(
+            search_kwargs={"k": k, "return_source_documents": True}
+        ),
+    )
+    # chain.retriever.return_source_documents = True
+
+    chat_history = []  # the simplest memory possible
+    while True:
+        if not query:
+            query = input("Prompt: ")
+        if query in ["quit", "q", "exit"]:
+            sys.exit()
+
+        # debugging the retrieval
+        if PERSIST:
+            # for the persisted chromadb
+            found_docs = await vectorstore.amax_marginal_relevance_search(
+                query, k=k, fetch_k=10
+            )
+        else:
+            # breakpoint()
+            found_docs = chain.retriever._get_relevant_documents(
+                query, run_manager=chain.callback_manager
+            )
+
+        for i, doc in enumerate(found_docs):
+            print(f"DEBUG: {i + 1}.", doc.page_content, "\n")
+
+        result = chain({"question": query, "chat_history": chat_history})
+        print(result["answer"])
+
+        chat_history.append((query, result["answer"]))
+        query = None
+
+
+import logging
+import os
+
+if __name__ == "__main__":
+    LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
+    logging.basicConfig(level=LOGLEVEL)
+    asyncio.run(loop(k=10))
